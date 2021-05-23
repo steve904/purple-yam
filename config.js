8ba3347e6ad4abb08b28a053f9f35fa85a8a2d7c
Stackbit <projects@stackbit.com>
2021-05-23 12:28:23 +0000

    initial commit by Stackbit

---
+++ config.js
@@ -0,0 +1,24 @@
+'use strict';
+
+module.exports = {
+  url: `https://abhaynikam.github.io/gatsby-nice-blog/`,
+  description: `A nice starter blog post boilerplate template built using Gatsby.`,
+  pathPrefix: '/',
+  title: `Nice Blogs`,
+  disqusShortname: '',
+  postsPerPage: 15,
+  googleAnalyticsId: '',
+  useKatex: false,
+  author: {
+    name: `Abhay Nikam`,
+    summary: `, Ruby on Rails and Frontend developer from Pune, India.`,
+    social: {
+      twitter: `abhaynikam13`,
+      github: `abhaynikam`,
+      email: `nikam.abhay1@gmail.com`,
+      linkedin: `abhaynikam`,
+      facebook: ``,
+      instagram: ``,
+    },
+  }
+};
